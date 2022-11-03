f00dd648b39afc4c901d0c43963b633608825749
sertannavdann@gmail.com <sertannavdann@gmail.com>
2022-11-03 16:45:09 -0700

    The final version of the Class based game programming

---
+++ Shooter/MyBTTask_ClearBlackBoardValue.h
@@ -0,0 +1,22 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/Tasks/BTTask_BlackboardBase.h"
+#include "MyBTTask_ClearBlackBoardValue.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SHOOTER_API UMyBTTask_ClearBlackBoardValue : public UBTTask_BlackboardBase
+{
+	GENERATED_BODY()
+
+public:
+	UMyBTTask_ClearBlackBoardValue();
+
+protected:
+	virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
+};
---
+++ Shooter/ShooterGameModeBase.cpp
@@ -0,0 +1,9 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+
+#include "ShooterGameModeBase.h"
+
+void AShooterGameModeBase::PawnKilled(APawn* PawnKilled)
+{
+
+}
